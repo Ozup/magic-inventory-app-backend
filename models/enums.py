cefba02aa0e96bc90daca6ce11922cb4e73f763b
Samuel Giraldo <ozupacademico@gmail.com>
2026-05-27 19:34:42 -0500

    add album collection type

--- models/enums.py
+++ models/enums.py
@@ -1,21 +1,48 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime, Enum, UniqueConstraint
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Boolean,
+    ForeignKey,
+    DateTime,
+    Enum,
+    UniqueConstraint
+)
+
 from sqlalchemy.orm import relationship
+
 from database import Base
+
 from datetime import datetime
 
-####################################
-# Para el tipo de la colección debo restrigir las posibilidades.
-####################################
 import enum
 
+
+####################################
+# Collection Types
+####################################
+
 class CollectionType(enum.Enum):
+
     ALBUM = "ALBUM"
+
     DECK = "DECK"
+
     BINDER = "BINDER"
+
+    COLLECTION = "COLLECTION"
+
+
+####################################
+# Deck Formats
 ####################################
 
 class DeckFormat(enum.Enum):
+
     COMMANDER = "COMMANDER"
+
     STANDARD = "STANDARD"
+
     MODERN = "MODERN"
+
     PAUPER = "PAUPER"
